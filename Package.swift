bd9a57dd8381960799d6a719ff32f95e3a8522d5
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-17 19:43:45 -0400

    Move files (#1)
    Getting ready to add React app

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         // 🐘 Fluent driver for Postgres.
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        // SQLite for faster tests
+        // SQLite for faster tests.
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
     ],
     targets: [
@@ -29,6 +29,11 @@ let package = Package(
                 .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             ],
+            path: "VaporApp/Sources",
+            resources: [
+                .copy("../Public/"),
+                .copy("../Resources/")
+            ],
             swiftSettings: swiftSettings
         ),
         .testTarget(
@@ -37,6 +42,7 @@ let package = Package(
                 .target(name: "App"),
                 .product(name: "XCTVapor", package: "vapor"),
             ],
+            path: "VaporApp/Tests/AppTests",
             swiftSettings: swiftSettings
         )
     ]
