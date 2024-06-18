5115f37cf533308dc97f04c02fffea7d4a3f10bf
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-18 14:30:34 -0400

    Comment out "home" route, get running again

--- VaporApp/Sources/App/routes.swift
+++ VaporApp/Sources/App/routes.swift
@@ -1,6 +1,5 @@
 import Fluent
 import Vapor
-import Leaf
 
 func routes(_ app: Application) throws {
   app.get { req async in
@@ -11,21 +10,21 @@ func routes(_ app: Application) throws {
     "Hello, world!"
   }
 
-  app.get("home") { req async -> String in
-    let fileIO = req.application.fileio
-    let path = req.application.directory.publicDirectory + "index.html"
+//   app.get("home") { req async -> String in
+//     let fileIO = req.application.fileio
+//     let path = req.application.directory.publicDirectory + "index.html"
 
-    return fileIO.openFile(path: path, mode: .read, eventLoop: req.eventLoop).flatMap {
-      (handle, region) in
-      let response = Response(status: .ok, headers: ["Content-Type": "text/html; charset=utf-8"])
-      return fileIO.read(fileRegion: region, allocator: .init(), eventLoop: req.eventLoop).map {
-        buffer in
-        response.body = .init(buffer: buffer)
-        try? handle.close()
-        return response
-      }
-    }
-  }
+//     return fileIO.openFile(path: path, mode: .read, eventLoop: req.eventLoop).flatMap {
+//       (handle, region) in
+//       let response = Response(status: .ok, headers: ["Content-Type": "text/html; charset=utf-8"])
+//       return fileIO.read(fileRegion: region, allocator: .init(), eventLoop: req.eventLoop).map {
+//         buffer in
+//         response.body = .init(buffer: buffer)
+//         try? handle.close()
+//         return response
+//       }
+//     }
+//   }
 
   try app.register(collection: TodoController())
 }
