4cc32358bd3a66028b129a85a61bd7c890eb80cc
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-18 19:09:39 -0400

    Clickable button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
+  const handleButtonClick = () => {
+    console.log('Button clicked!');
+    alert('Button clicked!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +16,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          <Button type="primary" onClick={handleButtonClick}>Yo God!</Button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
