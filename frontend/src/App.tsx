885ce91392f57dd7aadb8e8a89fac716ee0d8f28
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-19 10:45:01 -0400

    Load links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Layout, Input, List, Typography } from 'antd';
@@ -7,10 +7,29 @@ const { Header, Content, Footer, Sider } = Layout;
 
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('input');
+  const [links, setLinks] = useState<Link[]>([])
   const handleButtonClick = () => {
     console.log('Button clicked!');
     alert('Button clicked!');
+  };
+
+  useEffect(() => {
+    onAppear();
+  })
+  const onAppear = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8080/links', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const result = await response.json();
+      setLinks(result);
+    } catch (error) {
+      console.error('Error fetching links:', error);
+    }
   };
   const submitLinkClick = async () => {
     if (inputValue) {
@@ -43,7 +62,11 @@ function App() {
     "cat",
     "bird"
   ];
-
+  type Link = {
+    id: string,
+    url: string,
+    title: string
+  }
 
   return (
     <div className="App">
@@ -52,14 +75,14 @@ function App() {
         <Content>
           <Input size="large" placeholder="Enter Link" value={inputValue} onChange={handleInputChange} />
           <Button type="primary" onClick={submitLinkClick}>Submit</Button>
-          <List
+          <List<Link>
             header={<div>Header</div>}
             footer={<div>Footer</div>}
             bordered
-            dataSource={data}
-            renderItem={(item) => (
+            dataSource={links}
+            renderItem={(link) => (
               <List.Item>
-                {item}
+                {link.title}: <a href ={link.url}>link</a>
               </List.Item>
             )}
           />
